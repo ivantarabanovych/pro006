86ce90e96594031a9bcf0ab4299eec9c77c671a6
ivantarabanovych <vanjokcitet334@gmail.com>
2024-07-03 19:39:48 +0300

    q3

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-let numString = "42";
-let numInt = parseInt(numString);
-console.log(numInt);
-console.log(typeof numInt);
-
-console.log(numString)
-console.log(typeof numString)
-
-let numFloat = parseFloat(numString);
-console.log(numFloat);
-console.log(typeof numFloat);
+let isLoggedOut = false;
+let loggedInString = String(isLoggedOut);
+console.log(loggedInString);
