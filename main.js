2228491fb6083029b242f6f143f5bc45aeff4140
ivantarabanovych <vanjokcitet334@gmail.com>
2024-07-03 19:33:38 +0300

    q2

---
+++ main.js
@@ -0,0 +1,11 @@
+let numString = "42";
+let numInt = parseInt(numString);
+console.log(numInt);
+console.log(typeof numInt);
+
+console.log(numString)
+console.log(typeof numString)
+
+let numFloat = parseFloat(numString);
+console.log(numFloat);
+console.log(typeof numFloat);
